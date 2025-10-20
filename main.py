78872f6811e2651eff9fd42b73cc17205567ec66
Julianlmz <jsleal77@ucatolica.edu.co>
2025-10-20 13:02:48 -0500

    Creacion de main.py, se despliega en el Localhost

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from db import create_tables
+import Usuario
+
+app = FastAPI(title="NutriBox API")
+
+@app.on_event("startup")
+def on_startup():
+    create_tables(app)
+
+app.include_router(Usuario.router, tags=["Usuarios"], prefix="/usuarios")
+
+@app.get("/")
+async def root():
+    return {"message": "API de NutriBox funcionando"}
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hola {name}, bienvenido a NutriBox 👋"}
