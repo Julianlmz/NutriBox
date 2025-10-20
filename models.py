424d81fed27bac58b531331e3545e123e2fd513b
Julianlmz <jsleal77@ucatolica.edu.co>
2025-10-20 17:42:25 -0500

    Actualizacion del perfil Usuario con Rol(Padre - Hijo)

--- models.py
+++ models.py
@@ -1,16 +1,16 @@
-from pydantic import BaseModel
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import SQLModel, Field
+from typing import Optional
 
 class UsuarioBase(SQLModel):
     nombre: str = Field(description="Nombre del usuario")
     apellido: str = Field(description="Apellido del usuario")
     localidad: str = Field(description="Localidad del usuario")
     edad: int = Field(description="Edad")
+    rol: str = Field(description="Rol del usuario (Padre o Hijo)")
 
 class Usuario(UsuarioBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     cedula: int = Field(unique=True, description="Cédula del usuario")
 
 class UsuarioCreate(UsuarioBase):
     cedula: str = Field(description="Cédula del usuario")
-
