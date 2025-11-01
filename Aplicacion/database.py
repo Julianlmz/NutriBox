820cdf77f694d3443434fdbbe8790f89a0f7edc1
Julianlmz <jsleal77@ucatolica.edu.co>
2025-11-01 11:50:26 -0500

    Codigo mas limpio y legible en database.py

---
+++ Aplicacion/database.py
@@ -0,0 +1,16 @@
+from sqlmodel import Session, create_engine, SQLModel
+from fastapi import Depends
+from typing import Annotated
+
+engine = create_engine('sqlite:///NutriBox.db')
+
+
+def create_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
