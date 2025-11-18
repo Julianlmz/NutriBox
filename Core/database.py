b9dcbe12616ecce6041d4938c9d4f5f373e4d519
Julianlmz <jsleal77@ucatolica.edu.co>
2025-11-18 14:37:18 -0500

    Cambios en la base de datos

--- Core/database.py
+++ Core/database.py
@@ -4,22 +4,29 @@ from fastapi import Depends
 from typing import Annotated
 
 DATABASE_URL = os.environ.get("POSTGRESQL_ADDON_URI")
-connect_args = {}
+connect_args = {} # Se inicializa vacío
 
 if DATABASE_URL is None:
+    # MODO DESARROLLO (SQLite)
     print("MODO DESARROLLO: Conectando a base de datos SQLite local (NutriBox.db)")
     DATABASE_URL = "sqlite:///./NutriBox.db"
     connect_args = {"check_same_thread": False}
 else:
-    print("MODO PRODUCCIÓN: Conectando a base de datos de Clever Cloud")
+    # MODO PRODUCCIÓN (Azure PostgreSQL)
+    print("MODO PRODUCCIÓN: Conectando a base de datos en Azure")
+
+    # Asegura que se use el driver 'postgresql://'
     if DATABASE_URL.startswith("postgres://"):
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
+    # ¡Importante! Azure requiere conexiones SSL
+    connect_args = {"sslmode": "require"}
+
+# Crea el 'engine' con los argumentos de conexión correctos (ya sea para SQLite o Azure)
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 def create_tables():
     SQLModel.metadata.create_all(engine, checkfirst=True)
-
 
 def get_session():
     with Session(engine) as session:
